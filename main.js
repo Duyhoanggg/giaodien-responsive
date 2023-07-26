862a86fc5001f2120e25d351425d5c2f801e58f0
Hoangldph21518 <Hoangldph21518@fpt.edu.vn>
2023-07-26 13:08:12 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+            const button = document.getElementById("button");
+            const menu = document.getElementById("main-menu");
+
+            button.addEventListener("click", function () {
+                menu.classList.toggle("hidden");
+            });
+        
